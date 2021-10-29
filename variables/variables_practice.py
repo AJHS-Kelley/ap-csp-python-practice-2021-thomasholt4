2b1e1aac571bf6b63395a9c58ee858d3baeef8c2
Thomas Holt <73533784+thomasholt4@users.noreply.github.com>
2021-10-29 13:03:03 -0400

    Version 0.1
    Added three variables. Integer, Float, String.

---
+++ variables/variables_practice.py
@@ -0,0 +1,8 @@
+# Variables Practice, Thomas Holt, 10/29/21, 1:01pm, version 0.1
+
+
+# Basic Variables
+
+my_int = -15 # INTERGERS, any +/- whole number and 0.
+my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal.
+my_string = "How much wood could a wood chuck if a wood chuck could chuck wood?" 
