90b31ce7699a048f55fc8e46d7caadcf080ac370
Thomas Holt <73533784+thomasholt4@users.noreply.github.com>
2021-10-29 13:19:54 -0400

    Version 0.2
    Added a way to get a users input.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Thomas Holt, 10/29/21, 1:01pm, version 0.1
+# Variables Practice, Thomas Holt, 10/29/21, 1:16pm, version 0.2
 
 
 # Basic Variables
@@ -6,3 +6,16 @@
 my_int = -15 # INTERGERS, any +/- whole number and 0.
 my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal.
 my_string = "How much wood could a wood chuck if a wood chuck could chuck wood?" 
+
+print(my_int)
+print(my_float)
+print(my_string)
+
+my_int = input("How many fingers am I holding up? \n")
+print(my_int)
+
+my_float = input("What is pie? \n")
+print(my_float)
+
+my_string = input("What is your name? \n")
+print(my_string)
